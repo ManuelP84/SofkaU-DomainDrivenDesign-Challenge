cd9592a372302d07efc1e40c21cc4abafb4df1cc
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-21 17:34:54 -0500

    Test 10 ---- Event triggered test. CreateItem event trigger the CreateOffer event. Adjustments. Running fine!

--- src/main/java/com/sofkau/domainDrivenDesignChallenge/usecase/NotifyOfferProcessUseCase.java
+++ src/main/java/com/sofkau/domainDrivenDesignChallenge/usecase/NotifyOfferProcessUseCase.java
@@ -4,7 +4,6 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.ResponseEvents;
 import co.com.sofka.business.support.TriggeredEvent;
 import com.sofkau.domainDrivenDesignChallenge.domain.cart.Cart;
-import com.sofkau.domainDrivenDesignChallenge.domain.cart.Offer;
 import com.sofkau.domainDrivenDesignChallenge.domain.cart.events.ItemCreated;
 import com.sofkau.domainDrivenDesignChallenge.domain.cart.values.Discount;
 import com.sofkau.domainDrivenDesignChallenge.domain.cart.values.OfferId;
@@ -15,7 +14,10 @@ public class NotifyOfferProcessUseCase extends UseCase<TriggeredEvent<ItemCreate
     @Override
     public void executeUseCase(TriggeredEvent<ItemCreated> itemCreatedTriggeredEvent) {
         var event = itemCreatedTriggeredEvent.getDomainEvent();
-        var cart = Cart.from(CartId.of(event.aggregateRootId()), repository().getEventsBy(event.aggregateRootId()));
+        var cart = Cart.from(
+                CartId.of(event.aggregateRootId()),
+                repository().getEventsBy(event.aggregateRootId())
+        );
         cart.createOffer(
                 OfferId.of("12345"),
                 ProductId.of("354303"),
